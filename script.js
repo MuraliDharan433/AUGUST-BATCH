db0146c38d421910464f32da42b40724d185fa76
MuraliDharan433 <jackmurali433@gmail.com>
2024-10-15 15:17:27 +0530

    Update files

--- script.js
+++ script.js
@@ -1,7 +1,177 @@
-let a = 10;
-let b = 20;
+// let a = 10;
+// let b = 20;
 
-let total = a + b
+// let total = a + b
 
-console.log(total);
+// console.log(total);
+
+// let a = 13;
+
+// let b = 2;
+
+// let aaa = a = a + 3 //a += 3
+
+// let opra = a - 1 //a--\
+
+// let name = "Karthik"
+
+// let role = "WEB DEV";
+
+// let dob = 2000
+
+// let findage = 2024 - dob
+
+
+// // console.log("I am " + name + " I am a " + role + " My Age is " + findage);
+
+// console.log(`I am ${name} I am a ${role} My Age is ${2024 - dob}`);
+
+
+// console.log();
+
+
+
+// let age = 50;
+
+// if(age >= 18 ){
+//      console.log("it if stament");
+     
+//      if(age < 50){
+//           console.log("Eligible For Voting");
+//      } else{
+//           console.log("Else statement");
+          
+//      }   
+//      console.log("it if stament");
+     
+     
+// }else if(age >= 50){
+//      console.log('Senior Citizen');
+     
+// }else if(age == 0){
+//      console.log("plese Enter age");
+     
+// }
+// else{
+//      console.log("Not Eligible For Voting");
+     
+// }
+// let time = 9
+
+// if (time < 10) {
+//      greeting = "Good morning";
+//    } else if (time < 16) {
+//      greeting = "Good day";
+//    } else {
+//      greeting = "Good evening";
+//    }
+
+//    console.log(greeting);
+   
+
+
+let arr = [1,2,3,4,5]
+let obj = {id:1,name:'murali',age:21,role:'web Dev'}
+let cars = ["BMW", "Volvo", "Mini"];
+let name = 'Muralidharan'
+// console.log(arr.length);
+
+// for(let i = 0; i < 100;i++){
+//      console.log(i)
+     
+// }
+
+// for(let item in obj){
+//      console.log(item,obj[item]);
+     
+// }
+
+
+// for(let data of cars){
+//      console.log(data);
+     
+// }
+
+// for(let char of name){
+//      console.log(char);
+     
+// }
+
+// let i = 1;
+
+// do{
+//      console.log(i);
+//      i++;
+     
+// }while(i < 10)
+// let day = prompt('Enter the day in num')
+// console.log(typeof(day));
+
+
+
+// switch(Number(day)){
+//      case 1:
+//           console.log('MON');
+//           break;
+//      case 2:
+//           console.log("TUS");
+//           break
+//      case 3:
+//           console.log("WED");
+//           break
+//      case 4:
+//           console.log("THURS");
+//           break
+//      case 5:
+//           console.log("FRI");
+//           break
+//      case 6:
+//           console.log("SAT");
+//           break
+//      case 7:
+//           console.log("SUN"); 
+//           break
+//      default:
+//           console.log("Enter Valid Number");
+//           break
+                  
+// }
+
+
+
+// let age = 20;
+
+// var vote = age>18? 'Eligible':'Not Eligible'
+// console.log(vote);
+
+// if(age > 18){
+//      console.log("Eligible");
+     
+// }else{
+//      console.log('Not Eligible');
+     
+// }
+// function changeName() {
+     
+// }   
+
+// let changeName = function(){
+
+// }
+
+let text = document.getElementById('text')
+
+
+let changeName = ()=>{
+     
+     text.textContent = 'My name is JavaScript'
+     text.style.color = 'red'
+     text.style.fontSize = '50px'
+     
+     
+}
+
+
+
+
 
